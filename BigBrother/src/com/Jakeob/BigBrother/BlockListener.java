336f8ae58282c422848cb5f112cd5e667335d7fb
Sean Valley <valleydsean@gmail.com>
2019-02-21 22:04:14 -0500

    Exploded blocks now register but the server freezes while inserting

--- BigBrother/src/com/Jakeob/BigBrother/BlockListener.java
+++ BigBrother/src/com/Jakeob/BigBrother/BlockListener.java
@@ -1,12 +1,15 @@
 package com.Jakeob.BigBrother;
 
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class BlockListener implements Listener{
 	private SQLHandler sqlh;
@@ -35,10 +38,16 @@ public class BlockListener implements Listener{
 	
 	//Blocks exploding still aren't registered to database
 	@EventHandler
-	public void onBlockExplode(BlockExplodeEvent event) {
-		Material type = event.getBlock().getType();
-		Location loc = event.getBlock().getLocation();
+	public void onBlockExplode(EntityExplodeEvent event) {
+		BigBrother.logger.info("Ran explode event");
+		List<Block> blockList = event.blockList();
 		
-		sqlh.addEntry("EXPLOSION", "removed", type, loc);
+		for(Block block : blockList) {
+			BigBrother.logger.info("looped");
+			Material type = block.getType();
+			Location loc = block.getLocation();
+			
+			sqlh.addEntry("EXPLOSION", "removed", type, loc);
+		}
 	}
 }
